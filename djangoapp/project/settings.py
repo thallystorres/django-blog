91e664f154e96059c7aad26e3424f9ad764725ad
thallys <thallys.auugusto.torres@gmail.com>
2025-04-21 23:46:56 -0300

    criando site_setup e registrando no admin do django

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'site_setup',
     'blog',
     'django.contrib.admin',
     'django.contrib.auth',
--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo 'Executando migrate.sh'
+makemigrations.sh
 python manage.py migrate --noinput
