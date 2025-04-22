284fe38f232305683a038b555bb769d568315ecb
thallys <thallys.auugusto.torres@gmail.com>
2025-04-22 11:44:31 -0300

    Criando foreignkey entre SiteSetup e MenuLink e adicionando TabularInline

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -2,17 +2,16 @@ from django.contrib import admin
 from site_setup import models
 from django.http import HttpRequest
 
-@admin.register(models.MenuLink)
-class MenuLink(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path',
-    list_display_links = 'id', 'text', 'url_or_path',
-    search_fields = 'id', 'text', 'url_or_path',
-    ordering = '-id',
+
+class MenuLinkInline(admin.TabularInline):
+    model = models.MenuLink
+    extra = 1
 
 
 @admin.register(models.SiteSetup)
-class SiteSetup(admin.ModelAdmin):
+class SiteSetupAdmin(admin.ModelAdmin):
     list_display = 'title', 'description',
+    inlines = MenuLinkInline,
 
     def has_add_permission(self, request: HttpRequest) -> bool:
         return not models.SiteSetup.objects.exists()
--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,20 +1,6 @@
 from django.db import models
 
 
-class MenuLink(models.Model):
-
-    class Meta:
-        verbose_name = "Menu Link"
-        verbose_name_plural = "Menu Links"
-
-    text = models.CharField(max_length=50)
-    url_or_path = models.CharField(max_length=2048)
-    new_tab = models.BooleanField(default=False)
-
-    def __str__(self) -> str:
-        return self.text
-
-
 class SiteSetup(models.Model):
     class Meta:
         verbose_name = 'Setup'
@@ -32,3 +18,21 @@ class SiteSetup(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class MenuLink(models.Model):
+
+    class Meta:
+        verbose_name = "Menu Link"
+        verbose_name_plural = "Menu Links"
+
+    text = models.CharField(max_length=50)
+    url_or_path = models.CharField(max_length=2048)
+    new_tab = models.BooleanField(default=False)
+    site_setup = models.ForeignKey(
+        SiteSetup, on_delete=models.CASCADE,
+        blank=True, null=True, default=None
+        )
+
+    def __str__(self) -> str:
+        return self.text
