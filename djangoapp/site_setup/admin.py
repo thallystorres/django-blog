952419d1c199463efc6970ca1265b3f1c75b5a6d
thallys <thallys.auugusto.torres@gmail.com>
2025-04-22 11:23:23 -0300

    Criando model SiteSetup e registrando no admin

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from site_setup import models
-
+from django.http import HttpRequest
 
 @admin.register(models.MenuLink)
 class MenuLink(admin.ModelAdmin):
     list_display = 'id', 'text', 'url_or_path',
     list_display_links = 'id', 'text', 'url_or_path',
     search_fields = 'id', 'text', 'url_or_path',
-    ordering = 'id',
+    ordering = '-id',
+
+
+@admin.register(models.SiteSetup)
+class SiteSetup(admin.ModelAdmin):
+    list_display = 'title', 'description',
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return not models.SiteSetup.objects.exists()
