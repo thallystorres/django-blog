3ff9194cb58e5d44ffad0be49bc5872e8ac489ec
thallys <thallys.auugusto.torres@gmail.com>
2025-04-23 17:50:49 -0300

    Focando no html e css dos templates

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+posts = list(range(1000))
 
 
 def index(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request=request,
-        template_name='blog/pages/index.html'
+        template_name='blog/pages/index.html',
+        context={
+            'page_obj': page_obj
+            },
+    )
+
+
+def page(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request=request,
+        template_name='blog/pages/page.html',
+        context={
+            # 'page_obj': page_obj
+            },
+    )
+
+
+def post(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request=request,
+        template_name='blog/pages/post.html',
+        context={
+            # 'page_obj': page_obj
+            },
     )
