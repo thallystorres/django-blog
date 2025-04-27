e130d027cf9921859214a3285ce06792bcfbf44a
thallys <thallys.auugusto.torres@gmail.com>
2025-04-27 15:39:05 -0300

    Criando models para tags e categories

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog import models
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug',
+    list_display_links = 'name',
+    search = 'id', 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('name',),
+    }
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug'
+    list_display_links = 'name',
+    search = 'id', 'name', 'slug'
+    ordering = '-id',
+    list_per_page = 10
+    prepopulated_fields = {
+        "slug": ('name',),
+    }
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from utils.rands import new_slugify
 
-# Create your models here.
+
+class Tag(models.Model):
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+    name = models.CharField(max_length=55)
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        default=None,
+        blank=True,
+        null=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=55)
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        blank=True,
+        default=True,
+        null=True,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slugify(self.name)
+            return super().save(*args, **kwargs)
